aac1702a45eebeaa8345b768f15a944a526594e0
maciub1 <92275332+maciub1@users.noreply.github.com>
2022-11-08 15:19:31 +0100

    init

---
+++ app/src/main/java/com/example/mobilki/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mobilki;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+
+                    Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
+                    finishAffinity();
+                    startActivity(intent);
+                }
+
+            }
+        };
+    }
+}
