0b3cd2d886ea3b0ac1134c7f971ba821d2c40e09
maciub1 <92275332+maciub1@users.noreply.github.com>
2022-11-15 14:45:09 +0100

    Skończony ekran logowania

--- app/src/main/java/com/example/mobilki/MainActivity.java
+++ app/src/main/java/com/example/mobilki/MainActivity.java
@@ -3,14 +3,51 @@ package com.example.mobilki;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private DatabaseReference mDatabaseUsers;
+
+    private ProgressDialog mProgress;
+
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = "LoginActivity";
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +55,173 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-        mAuthListener=new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser() != null){
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabaseUsers.keepSynced(true);
+        mProgress = new ProgressDialog(this);
+        SignInButton mGoogleBtn = findViewById(R.id.googleBtn);
+        mEmailField = findViewById(R.id.emailField);
+        mPasswordField = findViewById(R.id.passwordField);
 
-                    Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
-                    finishAffinity();
-                    startActivity(intent);
+        Button mLoginBtn = findViewById(R.id.loginBtn);
+        Button mRegisterBtn = findViewById(R.id.registerBtn);
+        Button mResetBtn = findViewById(R.id.paswordResetBtn);
+        Button mGuestBtn = findViewById(R.id.guestLogin);
+        mAuthListener= firebaseAuth -> {
+            if(firebaseAuth.getCurrentUser() != null){
+
+                Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
+                finishAffinity();
+                startActivity(intent);
+            }
+
+        };
+
+        mLoginBtn.setOnClickListener(view -> startSignIn());
+
+        mGoogleBtn.setOnClickListener(view -> signIn());
+
+        mRegisterBtn.setOnClickListener(view -> {
+            Intent registerIntent= new Intent(MainActivity.this,RegisterActivity.class);
+            registerIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(registerIntent);
+
+        });
+
+        mResetBtn.setOnClickListener(view -> {
+            Intent resetIntent = new Intent(MainActivity.this,ResetPasswordActivity.class);
+            resetIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(resetIntent);
+
+        });
+
+        mGuestBtn.setOnClickListener(view -> guestLogin());
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("98236990119-sr3g4n1acr1584an6fssfpjep86ejm6g.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void guestLogin() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Co poszło nie tak",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            mProgress.setMessage("Logowanie ... ");
+            mProgress.show();
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                //Toast.makeText(MainActivity.this, "Logowanie powiodo się", Toast.LENGTH_LONG).show();
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                mProgress.dismiss();
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(MainActivity.this, "Logowanie nie powiodło się", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn(){
+        String email = mEmailField.getText().toString().trim();
+        String password = mPasswordField.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mProgress.setMessage("Sprawdzam dane logowania ...");
+            mProgress.show();
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    mProgress.dismiss();
+                    checkUserExist();
+
                 }
+                else{
+                    mProgress.dismiss();
+                    Toast.makeText(MainActivity.this, "Błędne dane logowania", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else{
+            Toast.makeText(MainActivity.this, "Wprowadź dane", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void checkUserExist() {
+        String user_id = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+
+        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.hasChild(user_id)){
+                    Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                } else {
+                    Intent setupIntent = new Intent(MainActivity.this,SetupActivity.class);
+                    setupIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(setupIntent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        };
+        });
     }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        mProgress.dismiss();
+                        Log.d(TAG, "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        mProgress.dismiss();
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Logowanie nie powiodło się", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/mobilki/SetupActivity.java
@@ -0,0 +1,13 @@
+package com.example.mobilki;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
